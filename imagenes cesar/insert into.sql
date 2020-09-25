65b899130c8cdaa3b05ce50a858f55770deb07ad
cesargq <70243978+cesargarzon19@users.noreply.github.com>
2020-09-24 21:39:50 -0500

    Add files via upload

---
+++ imagenes cesar/insert into.sql
@@ -0,0 +1,32 @@
+create database if not exists medio_ambiente2;
+
+use medio_ambiente2;
+
+create table contaminacion3(
+      id_conta int primary key auto_increment not null,
+     porcentaje decimal(3) not null,
+     consecuencias varchar(50),
+     causas varchar(50)
+);
+
+create table contaminacion_atmosferica(
+      id_atm int primary key auto_increment not null,
+     porcentaje decimal(3) not null,
+     consecuencias varchar(50),
+     causas varchar(50),
+      idconta int,
+       constraint fk_conta foreign key(idconta)
+    references contaminacion3(id_conta)
+);
+
+insert into contaminacion3
+	values(0,"39","la desaparición de la biodiversidad","vertido de desechos industriales");
+
+insert into contaminacion3
+	values(0,"40","la desaparición los ecosistemas acuáticos"," aumento de las temperaturas");
+
+insert into contaminacion_atmosferica
+	values(0,"20","Consumo de combustible fósil"," Lluvia ácida",2);
+    
+insert into contaminacion_atmosferica
+	values(0,"37","Daño a la capa de ozono"," gases para refrigeración",2);
